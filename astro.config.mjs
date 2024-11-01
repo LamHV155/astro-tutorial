7722ba9f76d0b523a2a934d0bd9fc31f4eadaafd
viet.lamhoang <lamhv155@gmail.com>
2024-11-01 14:12:33 +0700

    feat: dark theme, try astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vietlamblog.netlify.app"
+  site: "https://vietlamblog.netlify.app",
+  integrations: [preact()]
 });
